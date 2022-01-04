ed5337171f9c17fdabe44b375acc67242e55aa64
Juan Bocanegra <jbocane6@gmail.com>
2022-01-03 21:22:05 -0500

    file 0 v.3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Using Puppet, create a file in /tmp.
 file { 'Holberton file':
-  ensure  =>  'present',
-  path  =>  '/tmp/school',
-  mode  =>  '0744',
-  owner =>  'www-data',
-  group =>  'www-data',
+  path    =>  '/tmp/school',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
   content =>  'I love Puppet'
 }
