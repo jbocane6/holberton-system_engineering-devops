0bbb3e32505e4c7d44a910fe33d58cfd62b712b8
Juan Bocanegra <jbocane6@gmail.com>
2022-01-03 21:47:09 -0500

    file 1 v.10

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Using Puppet, install puppet-lint.
-package { 'puppet_lint':
-  ensure => '2.5.0',
-  source => 'https://rubygems.org'
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
+  source   => 'https://rubygems.org',
 }
