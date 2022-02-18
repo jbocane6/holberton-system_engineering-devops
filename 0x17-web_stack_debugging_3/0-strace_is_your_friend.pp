10dd75c653d113c02b3d1b6bba1674f8a61ac0a2
Juan Bocanegra <jbocane6@gmail.com>
2022-02-18 07:16:15 -0500

    [UPDATE] file 0 v.3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Using strace, find out why Apache is returning 500 error. Fix & automate it
 exec { 'fix phpp':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => ['/usr/bin', '/bin']
