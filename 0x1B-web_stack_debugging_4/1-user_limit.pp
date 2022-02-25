201c23c9179c3f65c55e043147bde76437515eed
Juan Camilo Bocanegra Osorio <75183250+jbocane6@users.noreply.github.com>
2022-02-24 21:36:33 -0500

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,7 +0,0 @@
-# Change the OS configuration so that it is possible to login
-# with the holberton user and open a file without any error message.
-
-exec { 'fix ulimit':
-  command  => 'sed -i "56,57d" /etc/security/limits.conf',
-  provider => 'shell'
-}
